9278f3c62d2db0672fd1baaccd557df1dba468f9
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-03-20 19:14:31 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.42.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
